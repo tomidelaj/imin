b67b35c98da969bfcde12db82007fc606f64fae9
Primoz Hadalin <primoz.hadalin@xlab.si>
2015-08-18 10:29:20 +0200

    Add slug and slug validatio to group model

--- server/api/group/group.model.js
+++ server/api/group/group.model.js
@@ -5,8 +5,20 @@ var mongoose = require('mongoose'),
 
 var GroupSchema = new Schema({
   name: String,
+  slug: String,
   info: String,
   active: Boolean
 });
+
+GroupSchema.path('slug').validate(function(value) {
+  // Slug must not be longer than 20 chars and
+  // must be url friendly
+  if (value.length <= 20) {
+    return /^[a-z0-9-]+$/i.test(value);
+  }
+  else {
+    return false;
+  }
+}, 'Incorrect slug');
 
 module.exports = mongoose.model('Group', GroupSchema);
