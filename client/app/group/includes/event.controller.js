f83344bae27ca5bafafece16f8e942fcf1e249ab
Primoz Hadalin <primoz.hadalin@xlab.si>
2015-08-21 10:06:57 +0200

    Delete obsoloete files

--- client/app/group/includes/event.controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// TODO: This whole inlcudes thing needs some serious refactoring.
-
-angular.module('iminApp')
-  // TODO: change controller name
-  .controller('IncludeEventCtrl', function ($scope, $location, $stateParams, Events) {
-    $scope.events = Events.query();
-  });
