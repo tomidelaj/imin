6862580ad0ca492b539ee8eaef2768a476ac4f5a
Jure Polutnik <jure.polutnik@gmail.com>
2015-08-24 00:53:47 +0200

    Dynamic group header with navigation

---
+++ client/app/group/header/scrollclass.directive.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('iminApp')
+  .directive('scrollClass', function($window) {
+    return {
+      restrict: 'A',
+      link: function($scope, element, attrs) {
+        angular.element($window).bind('scroll', function() {
+          console.log(attrs);
+          if (this.pageYOffset > 150) {
+            element.addClass(attrs.scrollClass);
+          } else {
+            element.removeClass(attrs.scrollClass);
+          }
+          $scope.$apply();
+        });
+      }
+    };
+  });
