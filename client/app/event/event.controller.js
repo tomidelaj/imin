3a27cbb8ec03448d37685b4656c029509b806812
Primož Hadalin <hadalin.primoz@gmail.com>
2015-08-25 21:53:26 +0200

    Remove obsolete dependencies

--- client/app/event/event.controller.js
+++ client/app/event/event.controller.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('iminApp')
-  .controller('EventCtrl', function($scope, $stateParams, socket, Events, EventMessageFactory, EventParticipantFactory) {
+  .controller('EventCtrl', function($scope, $stateParams, Events) {
 
     $scope.initEvent = function() {
-      // Two ways of using this controller 
+      // Two ways of using this controller
       // a) event is passed from parent scope (ng-include)
       // b) event must be retrieved using state params
       if (!$scope.event) {
