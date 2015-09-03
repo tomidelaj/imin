ed0edc9bb40701130de546bd73eda9a09e4b87ec
Jure Polutnik <jure.polutnik@gmail.com>
2015-09-04 00:47:55 +0200

    Remember participant name

--- client/app/event/participants.controller.js
+++ client/app/event/participants.controller.js
@@ -1,17 +1,21 @@
 'use strict';
 
 angular.module('iminApp')
-  .controller('ParticipantsCtrl', function($scope, socket, EventParticipantFactory) {
+  .controller('ParticipantsCtrl', function($scope, $cookies, socket, EventParticipantFactory) {
     var init = function() {
       var EventParticipant = new EventParticipantFactory($scope.event._id);
       $scope.participants = EventParticipant.query();
       socket.syncUpdates('participant', $scope.participants);
+
+      $scope.participantName = $cookies.get('participantName');
 
       $scope.addParticipant = function() {
         var newParticipant = new EventParticipant({
           event: $scope.event._id,
           name: $scope.participantName
         });
+
+        $cookies.put('participantName', $scope.participantName);
 
         newParticipant.$save();
         $scope.participantName = '';
